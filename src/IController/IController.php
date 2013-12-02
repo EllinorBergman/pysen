9f5f5c456004ff254b8cdf84644e6f7edfdd1817
EllinorBergman <elli.bergman@gmail.com>
2013-12-02 15:26:54 +0000

    Refactor to simplify

--- src/IController/IController.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Interface for classes implementing a controller.
- *
- * @package PysenCore
- */
-interface IController {
-	public function Index();
-}
--- src/ISingleton/ISingleton.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Interface for classes implementing the singleton pattern.
- *
- * @package PysenCore
- */
-interface ISingleton {
-	public static function Instance();
-}
